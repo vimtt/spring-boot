ab3abd860a16702befb6345fa422d2171cd7f533
vim <vimeilar@126.com>
2017-12-17 22:10:57 +0800

    DEL(vim): 删除多余批处理

--- kiosk-spring-boot-master/git_init_push.cmd
+++
@@ -1,6 +0,0 @@
-echo "# spring-boot" >> README.md
-git init
-git add README.md
-git commit -m "first commit for kiosk-spring-boot"
-git remote add origin https://github.com/vimtt/spring-boot.git
-git push -u origin master
